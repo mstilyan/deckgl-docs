2623ff817075c6eb25d45af77a930e5cb44ad476
mstilyan <stilyan.mladenov@gmail.com>
2019-04-24 07:53:39 +0300

    added index.html

---
+++ main.js
@@ -0,0 +1,87 @@
+const app = {};
+
+app.renderBasicMapExample = function (elmId) {
+    // source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
+    const COUNTRIES =
+        'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_scale_rank.geojson'; //eslint-disable-line
+    const AIR_PORTS =
+        'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
+
+    const INITIAL_VIEW_STATE = {
+        latitude: 51.47,
+        longitude: 0.45,
+        zoom: 1,
+        bearing: 0,
+        pitch: 30
+    };
+
+    return new deck.DeckGL({
+        initialViewState: INITIAL_VIEW_STATE,
+        controller: true,
+        container: elmId,
+        layers: [
+            new GeoJsonLayer({
+                id: 'base-map',
+                data: COUNTRIES,
+                // Styles
+                stroked: true,
+                filled: true,
+                lineWidthMinPixels: 2,
+                opacity: 0.4,
+                getLineDashArray: [3, 3],
+                getLineColor: [60, 60, 60],
+                getFillColor: [200, 200, 200]
+            }),
+            new GeoJsonLayer({
+                id: 'airports',
+                data: AIR_PORTS,
+                // Styles
+                filled: true,
+                pointRadiusMinPixels: 2,
+                opacity: 1,
+                pointRadiusScale: 2000,
+                getRadius: f => 11 - f.properties.scalerank,
+                getFillColor: [200, 0, 80, 180],
+                // Interactive props
+                pickable: true,
+                autoHighlight: true,
+                onClick: info =>
+                    // eslint-disable-next-line
+                    info.object && alert(`${info.object.properties.name} (${info.object.properties.abbrev})`)
+            }),
+            new ArcLayer({
+                id: 'arcs',
+                data: AIR_PORTS,
+                dataTransform: d => d.features.filter(f => f.properties.scalerank < 4),
+                // Styles
+                getSourcePosition: f => [-0.4531566, 51.4709959], // London
+                getTargetPosition: f => f.geometry.coordinates,
+                getSourceColor: [0, 128, 200],
+                getTargetColor: [200, 0, 80],
+                getWidth: 1
+            })
+        ]
+    });
+}
+
+app.renderHelloWorldExample = function (elmId) {
+    return new deck.DeckGL({
+        container: elmId,
+        longitude: -122.45,
+        latitude: 37.8,
+        zoom: 12,
+        layers: [
+            new deck.ScatterplotLayer({
+                data: [
+                    { position: [-122.45, 37.8], color: [255, 0, 0], radius: 1000 }
+                ]
+            }),
+            new deck.TextLayer({
+                data: [
+                    { position: [-122.45, 37.8], text: 'Hello World' },
+                    { position: [-122.45, 37.796], text: 'Drag me :)' }
+                ]
+            })
+        ]
+    });
+}
